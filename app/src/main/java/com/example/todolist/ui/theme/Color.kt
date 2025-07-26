23063dd8a4143b6195b6d2c4defe541f135b5140
Daze346 <nikolajkrajnov052@gmail.com>
2025-07-26 11:22:32 +0300

    Delete not used color

--- app/src/main/java/com/example/todolist/ui/theme/Color.kt
+++ app/src/main/java/com/example/todolist/ui/theme/Color.kt
@@ -10,8 +10,6 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val JetBlack = Color(0x0F000000)
-//val White = Color(0xFFFFFFFF)
 
 val LightBlack = Color(0xFF1A1A1A)
 val DarkGray = Color(0xFF2F2F2F)
